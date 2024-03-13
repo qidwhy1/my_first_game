125b7d2ec96618ec703d157835855685080e16eb
pupsmann 12 <qa1480489@gmail.com>
2024-03-13 18:59:49 +0100

    philis commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 pygame.init()
 
 WIDTH, HEIGHT = 300, 500
-screen = pygame.display.set_mode(WIDTH, HEIGHT)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tower blox")
 
 clock = pygame.time.Clock()
@@ -19,13 +19,13 @@ block_image =  pygame.transform.scale(block_image, (50, 50))
 block_width, block_height = 50, 50
 block_x = WIDTH // 2 - block_width // 2
 block_y = 50
-block_speed = 4
+block_speed = 2
 block_direction = 1
 block_falling = False
 
 tower = [[WIDTH // 2 - block_width //2, HEIGHT - block_height]]
 tower_falling = False
-scroll_speed = 0,7
+scroll_speed = 0.7
 
 score = 0
 font = pygame.font.Font(None, 36)
@@ -42,19 +42,40 @@ while run:
     if block_falling:
         block_y += block_speed
 
-    upper_block = pygame.Rect(tower[-1 ][0], tower[-1][1], block_width, block_height)
-    if pygame.Rect(block_x, block_y, block_width, block_height).collide(upper_block):
-        block_falling = False
-        tower.append([block_x, block_y])
-        block_y = 50
-        score += 1
+        upper_block = pygame.Rect(tower[-1 ][0], tower[-1][1], block_width, block_height)
+        if pygame.Rect(block_x, block_y, block_width, block_height).colliderect(upper_block):
+            block_falling = False
+            tower.append([block_x, block_y])
+            block_y = 50
+            score += 1
+
+        else:
+            for block in tower[:-1]:
+                lower_blocks = pygame.Rect(block[0], block[1], block_width, block_height)
+                if pygame.Rect(block_x, block_y, block_width, block_height).colliderect(lower_blocks):
+                    tower_falling = True
+                    break
+            if block_y + block_height > HEIGHT:
+                run = False
+                print("GAME OVER YOU SUCKER")
 
     else:
-        for block in tower[:-1]:
-            lower_blocks = pygame.Rect(block[0], block[1], block_width, block_height)
-            if pygame.Rect(block_x, block_y, block_width, block_height).colliderect(lower_blocks):
-                tower_falling = True
-                break
-        if block_y + block_height > HEIGHT:
-            run = False
-            print("GAME OVER YOU SUCKER")
+        block_x += block_speed * block_direction
+        if block_x + block_width > WIDTH or block_x < 0:
+            block_direction *=-1
+        if len(tower) > 4:
+            for block in tower:
+                block[1] += scroll_speed
+        if tower_falling:
+            for block in tower:
+                block[0] += block_speed
+
+    screen.fill(WHITE)
+    for block in tower:
+        screen.blit(block_image, (block[0], block[1]))
+    screen.blit(block_image,(block_x, block_y))
+
+
+
+    pygame.display.flip()
+    clock.tick(FPS)
